d94b078d944272953778edf58c0618a1e2a6d4f1
Liam Helmer <liam.helmer@gmail.com>
2024-02-28 12:27:24 -0800

    test-pr-4 #in-progress #comment Commit to thaumazo/aks-dev - fix pr to review and add pip install to env.sh #transition in-progress

--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
 export PULUMI_CONFIG_PASSPHRASE=
 export GITHUB_OWNER=thaumazo
 
+pip install -r requirements.txt
+
 az login && \
     pulumi login azblob://iac?storage_account=thaumazodev || exit 1
 
